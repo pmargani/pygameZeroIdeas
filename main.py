181191ee6102a2ca233bc009bd772b44aeed6f77
Paul Marganian <pmargani@nrao.edu>
2022-08-07 16:35:38 -0400

    option added for using joysticks

--- main.py
+++ main.py
@@ -8,6 +8,21 @@ import pygame
 pygame.init()
 numJoysticks = pygame.joystick.get_count()
 print("Detected num Joysticks: ", numJoysticks)
+useJoysticks = numJoysticks != 0
+
+# init all the joysticks we have
+joysticks = []
+for i in range(numJoysticks):
+    j = pygame.joystick.Joystick(i)
+    j.init()
+    joysticks.append(j)
+
+# north button goes forward
+JOY_BTN_NORTH = 3     
+JOY_BTN_SOUTH = 6    
+JOY_BTN_EAST = 5     
+JOY_BTN_WEST = 2    
+JOY_BTN_CENTER = 4
 
 from Tank import Tank
 from Tank import Bullet
@@ -130,14 +145,25 @@ def update(time_interval):
     # *** Tank 1 Controls
     tank1 = getTankById(tanks, 1)
     if tank1 is not None:
-        controlTank(
-            tank1,
-            bullets,
-            keyboard.up,
-            keyboard.down,
-            keyboard.right,
-            keyboard.left,
-            keyboard.space)
+        if useJoysticks and numJoysticks > 0:
+            j = joysticks[0]
+            controlTank(
+                tank1,
+                bullets,
+                j.get_button(JOY_BTN_NORTH),
+                j.get_button(JOY_BTN_SOUTH),
+                j.get_button(JOY_BTN_EAST),
+                j.get_button(JOY_BTN_WEST),
+                j.get_button(JOY_BTN_CENTER))
+        else:
+            controlTank(
+                tank1,
+                bullets,
+                keyboard.up,
+                keyboard.down,
+                keyboard.right,
+                keyboard.left,
+                keyboard.space)
 
         # if keyboard.up or keyboard.down:
         #     if keyboard.up:
@@ -165,14 +191,25 @@ def update(time_interval):
     # *** Tank 2 Controls
     tank2 = getTankById(tanks, 2)
     if tank2 is not None:
-        controlTank(
-            tank2,
-            bullets,
-            keyboard.w,
-            keyboard.s,
-            keyboard.d,
-            keyboard.a,
-            keyboard.z)
+        if useJoysticks and numJoysticks > 1:
+            j = joysticks[1]
+            controlTank(
+                tank2,
+                bullets,
+                j.get_button(JOY_BTN_NORTH),
+                j.get_button(JOY_BTN_SOUTH),
+                j.get_button(JOY_BTN_EAST),
+                j.get_button(JOY_BTN_WEST),
+                j.get_button(JOY_BTN_CENTER))
+        else:    
+            controlTank(
+                tank2,
+                bullets,
+                keyboard.w,
+                keyboard.s,
+                keyboard.d,
+                keyboard.a,
+                keyboard.z)
 
         # if keyboard.w or keyboard.s:
         #     if keyboard.w:
@@ -199,25 +236,47 @@ def update(time_interval):
 
     tank3 = getTankById(tanks, 3)
     if tank3 is not None:
-        controlTank(
-            tank3,
-            bullets,
-            keyboard.i,
-            keyboard.k,
-            keyboard.j,
-            keyboard.l,
-            keyboard.m)
+        if useJoysticks and numJoysticks > 2:
+            j = joysticks[2]
+            controlTank(
+                tank3,
+                bullets,
+                j.get_button(JOY_BTN_NORTH),
+                j.get_button(JOY_BTN_SOUTH),
+                j.get_button(JOY_BTN_EAST),
+                j.get_button(JOY_BTN_WEST),
+                j.get_button(JOY_BTN_CENTER))
+        else:    
+            controlTank(
+                tank3,
+                bullets,
+                keyboard.i,
+                keyboard.k,
+                keyboard.j,
+                keyboard.l,
+                keyboard.m)
 
     tank4 = getTankById(tanks, 4)
     if tank4 is not None:
-        controlTank(
-            tank4,
-            bullets,
-            keyboard.t,
-            keyboard.g,
-            keyboard.h,
-            keyboard.f,
-            keyboard.v)
+        if useJoysticks and numJoysticks > 3:
+            j = joysticks[3]
+            controlTank(
+                tank4,
+                bullets,
+                j.get_button(JOY_BTN_NORTH),
+                j.get_button(JOY_BTN_SOUTH),
+                j.get_button(JOY_BTN_EAST),
+                j.get_button(JOY_BTN_WEST),
+                j.get_button(JOY_BTN_CENTER))
+        else:    
+            controlTank(
+                tank4,
+                bullets,
+                keyboard.t,
+                keyboard.g,
+                keyboard.h,
+                keyboard.f,
+                keyboard.v)
 
     # keep tanks off bricks
     for brick in bricks:
