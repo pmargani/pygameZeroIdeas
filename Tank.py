817837d5b70d81619766ab5137d31c5f79d16b36
maxoganian <62914182+maxoganian@users.noreply.github.com>
2022-08-14 15:25:48 -0400

    Changed ammo reup

--- Tank.py
+++ Tank.py
@@ -71,7 +71,7 @@ class Tank(Obj):
         self.maxAmmo = 10
         self.numAmmo = 7
         self.ammoCounter = 0
-        self.timeUntilAmmo = 900
+        self.timeUntilAmmo = 1000
 
         self.absSpeed = speed
         self.speed = 0.0
